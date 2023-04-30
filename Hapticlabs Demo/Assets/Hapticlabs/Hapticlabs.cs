d3bdabd1ad0a2efa8b35c696e853a480f0f77b3c
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2023-04-30 15:42:20 +0200

    Added commands explanation

--- Hapticlabs Demo/Assets/Hapticlabs/Hapticlabs.cs
+++ Hapticlabs Demo/Assets/Hapticlabs/Hapticlabs.cs
@@ -1,4 +1,65 @@
-using System.Collections;
+/* 
+ * Version 0.1.0, 2023-04-30, Kay van den Aker, Hapticlabs
+ * 
+ * This component helps sending data to the Hapticlabs Satellite using a Serial libraty built by Pierre Rossel. 
+ * From anywhere in your project you can call these function to command the Satellite
+ * I recommend using the prefab with the Serial Config attached also. Make sure you use 115200 as speed.
+ * 
+ * 1. Starting a track by name. 
+ * -------
+ * 
+ *   StartTrack("trackName");
+ * 
+ * - upload the tracks to the Satellite in Hapticlabs Studio
+ * - First parameter is the name of the track you created in Hapticlabs studio
+  * - Optional parameters are:
+ *   - queue: true/false (default false)
+ *     controls if the track stops whatever is currently playing or adds it to the queue
+ *   - looping: true/false (default false)
+ *     controls if the track should loop or not
+ *   
+ * 2. Start a vibration
+ * -------
+ * 
+ *   Hapticlabs.Vibrate("B", 0.5, 120, 200000);
+ *
+ * - First parameter is the channel "A", "B" or "AB"
+ * - Second parameter is the intensity between 0 and 1
+ * - Third parameter is the frequency between 1 and 400
+ * - Fourth parameter is the duration in ms
+ * - Optional parameters are:
+ *   - queue: true/false (default false)
+ *     controls if the vibration stops whatever is currently playing or adds it to the queue
+ *   - looping: true/false (default false)
+ *     controls if the vibration should loop or not
+ * 
+ * 3. Start a pulse
+ * -------
+ * 
+ *   Hapticlabs.Pulse("B", 0.5, 200000);
+ *
+ *   Only supported for Voice Coils!
+ * - First parameter is the channel "A", "B" or "AB"
+ * - Second parameter is the intensity between 0 and 1
+ * - Third parameter is the duration in ms
+ * - Optional parameters are:
+ *   - queue: true/false (default false)
+ *     controls if the vibration stops whatever is currently playing or adds it to the queue
+ *   - looping: true/false (default false)
+ *     controls if the vibration should loop or not
+ * 
+ * Troubleshooting the Serial
+ * ---------------
+ * 
+ * You may get the following error:
+ *     error CS0234: The type or namespace name `Ports' does not exist in the namespace `System.IO'. 
+ *     Are you missing an assembly reference?
+ * Solution: 
+ *     Menu Edit | Project Settings | Player | Other Settings | API Compatibility Level: .Net 2.0
+ * 
+ */
+ 
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
